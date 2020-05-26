432068adf006e6e9bd375e1c79439731c2fa6d10
kMutagene <kevin-schneider@mutagene.de>
2020-05-26 08:51:06 +0200

    Add script to build and publish site to gh-pages

--- build.fsx
+++ build.fsx
@@ -1,6 +1,8 @@
 #r "paket:
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
+nuget Fake.Api.Github
+nuget Fake.Tools.Git
 nuget Fake.Core.Target //"
 #load ".fake/build.fsx/intellisense.fsx"
 
@@ -13,12 +15,18 @@ open Fake.Core.TargetOperators
 
 Target.initEnvironment ()
 
+let gitOwner = "nfdi4plants"
+let gitHome = sprintf "%s/%s" "https://github.com" gitOwner
+let gitName = "AnnotationPrinciples"
+
+let sitePath = Path.getFullName "./src"
+let publicPath = Path.getFullName "./src/_public"
+
 let runDotNet cmd workingDir =
     let result =
         Fake.DotNet.DotNet.exec (Fake.DotNet.DotNet.Options.withWorkingDirectory workingDir) cmd ""
     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
-let sitePath = Path.getFullName "./src"
 
 Target.create "All" ignore
 
@@ -31,6 +39,21 @@ Target.create "GenerateLoadScript" (fun _ ->
 //    runDotNet "fornax clean" sitePath 
 //)
 
+Target.create "BuildSite" (fun _ ->
+    runDotNet "fornax build" sitePath
+)
+
+Target.create "ReleaseDocsToGhPages" (fun _ ->
+    let tempDocsDir = "temp/gh-pages"
+    Shell.cleanDir tempDocsDir |> ignore
+    Fake.Tools.Git.Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
+    Shell.copyRecursive publicPath tempDocsDir true |> printfn "%A"
+    Fake.Tools.Git.Staging.stageAll tempDocsDir
+    Fake.Tools.Git.Commit.exec tempDocsDir (sprintf "Update docs (%A)" System.DateTime.UtcNow)
+    Fake.Tools.Git.Branches.push tempDocsDir
+)
+
+
 Target.create "Watch" (fun _ ->
     runDotNet "fornax watch" sitePath
     )
@@ -38,5 +61,8 @@ Target.create "Watch" (fun _ ->
 //"Clean"
 "GenerateLoadScript"
     ==> "Watch"
+
+"BuildSite"
+    ==>"ReleaseDocsToGhPages"
 
 Target.runOrDefaultWithArguments "All"
