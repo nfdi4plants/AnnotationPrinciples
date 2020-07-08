e6d1d0267075f8ea22633a4683761a8e95c2f65f
Kevin Schneider <kevin-schneider@mutagene.de>
2020-07-08 22:13:39 +0200

    Update images and design

--- .paket/load/netstandard2.1/Newtonsoft.Json.fsx
+++ .paket/load/netstandard2.1/Newtonsoft.Json.fsx
@@ -1,3 +1,3 @@
 namespace PaketLoadScripts
 
-#r "C:\\Users\\schne\\.nuget\\packages\\newtonsoft.json\\12.0.3\\lib\\netstandard2.0\\Newtonsoft.Json.dll" 
+#r "C:\\Users\\kevin\\.nuget\\packages\\newtonsoft.json\\12.0.3\\lib\\netstandard2.0\\Newtonsoft.Json.dll" 
